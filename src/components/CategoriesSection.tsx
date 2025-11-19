c6cd91062fe8e72f7ac1d2145e4c864c20a644d9
psticea <psticea@gmail.com>
2025-11-19 15:51:28 +0200

    cleanup categories section

--- src/components/CategoriesSection.tsx
+++ src/components/CategoriesSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLanguage } from '../hooks/useLanguage';
 import { getTranslation } from '../utils/translations';
 import { categories } from '../data';
